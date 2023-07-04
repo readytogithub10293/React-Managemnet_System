6a6dca13bd2a8ef2e4fd967af485e693ff7f23cd
samuraijaxon88 <samuraijaxon88@gmail.com>
2023-07-04 20:29:49 +0900

    customeradd

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
+import CustomerAdd from './components/CustomerAdd';
+import { CircularProgress } from '@mui/material';
+
 
 const styles = theme => ({
   root: {
@@ -41,25 +44,63 @@ class App extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.customers ? this.state.customers.map(c => {
-              return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-            }) : ''}
-          </TableBody>
-        </Table>
-      </Paper>
+           <div>
+
+        <Paper className={classes.root}>
+
+          <Table className={classes.table}>
+
+            <TableHead>
+
+              <TableRow>
+
+                <TableCell>번호</TableCell>
+
+                <TableCell>이미지</TableCell>
+
+                <TableCell>이름</TableCell>
+
+                <TableCell>생년월일</TableCell>
+
+                <TableCell>성별</TableCell>
+
+                <TableCell>직업</TableCell>
+
+              </TableRow>
+
+            </TableHead>
+
+            <TableBody>
+
+              {this.state.customers ?
+
+                this.state.customers.map(c => {
+
+                  return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+
+                }) :
+
+                <TableRow>
+
+                  <TableCell colSpan="6" align="center">
+
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+
+                  </TableCell>
+
+                </TableRow>
+
+              }
+
+            </TableBody>
+
+          </Table>
+
+        </Paper>
+
+        <CustomerAdd/>
+
+      </div>
     );
   }
 }
